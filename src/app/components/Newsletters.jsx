ca386dbeaa6d25a687975ce633a6df7719ccfea2
mashrursakif <mashrursakifsouherdo@gmail.com>
2025-11-06 14:09:44 +0600

    Fix search to include title

--- src/app/components/Newsletters.jsx
+++ src/app/components/Newsletters.jsx
@@ -6,52 +6,52 @@ import Newsletter from './Newsletter';
 // import ImageWithFallback from './ImageWithFallback';
 
 export default function Newsletters({ newsletters, className }) {
-  const [query, setQuery] = useState('');
+	const [query, setQuery] = useState('');
 
-  const fuse = new Fuse(newsletters, {
-    keys: ['name', 'description', 'date'],
-    threshold: 0.4,
-  });
+	const fuse = new Fuse(newsletters, {
+		keys: ['title', 'description', 'date'],
+		threshold: 0.4,
+	});
 
-  const results = useMemo(
-    () => (query ? fuse.search(query).map((res) => res.item) : newsletters),
-    [query]
-  );
+	const results = useMemo(
+		() => (query ? fuse.search(query).map((res) => res.item) : newsletters),
+		[query]
+	);
 
-  return (
-    <div
-      className={`py-12 px-6 lg:px-10 w-auto md:w-[50vw] flex flex-col items-center overflow-visible ${className}`}
-    >
-      <div className='max-w-4xl'>
-        <div className='mb-8 flex flex-col items-center'>
-          <h2
-            className='text-3xl text-secondary text-center font-bold mb-6'
-            // id='newsletter'
-          >
-            Newsletters
-          </h2>
+	return (
+		<div
+			className={`py-12 px-6 lg:px-10 w-auto md:w-[50vw] flex flex-col items-center overflow-visible ${className}`}
+		>
+			<div className='max-w-4xl'>
+				<div className='mb-8 flex flex-col items-center'>
+					<h2
+						className='text-3xl text-secondary text-center font-bold mb-6'
+						// id='newsletter'
+					>
+						Newsletters
+					</h2>
 
-          <input
-            type='text'
-            className='outline-none text-lg px-5 py-3 border-solid border-2 border-primary w-full max-w-2xl rounded-4xl focus-border'
-            value={query}
-            placeholder='Search Newsletters'
-            onChange={(e) => setQuery(e.target.value)}
-          />
-        </div>
+					<input
+						type='text'
+						className='outline-none text-lg px-5 py-3 border-solid border-2 border-primary w-full max-w-2xl rounded-4xl focus-border'
+						value={query}
+						placeholder='Search Newsletters'
+						onChange={(e) => setQuery(e.target.value)}
+					/>
+				</div>
 
-        {results.length == 0 ? (
-          <p className='text-lg text-center text-primary'>
-            No Newsletters Found
-          </p>
-        ) : (
-          <div className='grid grid-cols-1 2xl:grid-cols-2 gap-6'>
-            {results?.map((newsletter) => (
-              <Newsletter key={newsletter.id} newsletter={newsletter} />
-            ))}
-          </div>
-        )}
-      </div>
-    </div>
-  );
+				{results.length == 0 ? (
+					<p className='text-lg text-center text-primary'>
+						No Newsletters Found
+					</p>
+				) : (
+					<div className='grid grid-cols-1 2xl:grid-cols-2 gap-6'>
+						{results?.map((newsletter) => (
+							<Newsletter key={newsletter.id} newsletter={newsletter} />
+						))}
+					</div>
+				)}
+			</div>
+		</div>
+	);
 }
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,71 +2,71 @@ import Link from 'next/link';
 import Tabs from './components/Tabs';
 
 function orderData(data) {
-  const newsletters = data.files.map((newsletter) => {
-    const dataArray = newsletter.name.replace(/\.pdf$/i, '').split('_');
+	const newsletters = data.files.map((newsletter) => {
+		const dataArray = newsletter.name.replace(/\.pdf$/i, '').split('_');
 
-    const title = dataArray.at(0).trim();
-    let description = '';
-    if (dataArray.length == 3) description = dataArray?.at(1)?.trim() || '';
+		const title = dataArray.at(0).trim();
+		let description = '';
+		if (dataArray.length == 3) description = dataArray?.at(1)?.trim() || '';
 
-    const date = dataArray?.at(-1)?.trim() || '';
-    const weekNumber = parseFloat(title.match(/Week\s+(\d+)/i)[1] || '0');
+		const date = dataArray?.at(-1)?.trim() || '';
+		const weekNumber = parseFloat(title.match(/Week\s+(\d+)/i)[1] || '0');
 
-    return {
-      id: newsletter.id,
-      title,
-      description,
-      date,
-      thumbnailLink: newsletter.thumbnailLink,
-      webViewLink: newsletter.webViewLink,
-      weekNumber,
-    };
-  });
+		return {
+			id: newsletter.id,
+			title,
+			description,
+			date,
+			thumbnailLink: newsletter.thumbnailLink,
+			webViewLink: newsletter.webViewLink,
+			weekNumber,
+		};
+	});
 
-  const orderedNewsletters = newsletters.sort(
-    (a, b) => b.weekNumber - a.weekNumber
-  );
-  return orderedNewsletters;
+	const orderedNewsletters = newsletters.sort(
+		(a, b) => b.weekNumber - a.weekNumber
+	);
+	return orderedNewsletters;
 }
 
 export default async function Home() {
-  const res = await fetch(
-    `https://www.googleapis.com/drive/v3/files?q=%27${process.env.GOOGLE_DRIVE_FOLDER_ID}%27+in+parents&key=${process.env.GOOGLE_DRIVE_API_KEY}&fields=files(id,name,description,webViewLink,thumbnailLink)`,
-    { cache: 'no-store' }
-  );
-  const data = await res.json();
+	const res = await fetch(
+		`https://www.googleapis.com/drive/v3/files?q=%27${process.env.GOOGLE_DRIVE_FOLDER_ID}%27+in+parents&key=${process.env.GOOGLE_DRIVE_API_KEY}&fields=files(id,name,description,webViewLink,thumbnailLink)`,
+		{ cache: 'no-store' }
+	);
+	const data = await res.json();
 
-  const newsletters = orderData(data);
-  const latestEdition = newsletters.at(0);
+	const newsletters = orderData(data);
+	const latestEdition = newsletters.at(0);
 
-  return (
-    <div className='md:min-h-screen flex flex-col md:flex-row overflow-visible md:overflow-hidden'>
-      <div className='bg-grad flex-1 flex flex-col justify-start items-center w-auto md:w-[50vw]'>
-        {/* <Header /> */}
+	return (
+		<div className='md:min-h-screen flex flex-col md:flex-row overflow-visible md:overflow-hidden'>
+			<div className='bg-grad flex-1 flex flex-col justify-start items-center w-auto md:w-[50vw]'>
+				{/* <Header /> */}
 
-        <div className='flex items-center justify-center flex-col py-24 px-10 max-w-screen sm:max-w-xl text-center md:mt-24'>
-          <h1 className='text-7xl font-bold mb-8 text-primary'>
-            Campus Chronicles
-          </h1>
+				<div className='flex items-center justify-center flex-col py-24 px-10 max-w-screen sm:max-w-xl text-center md:mt-24'>
+					<h1 className='text-7xl font-bold mb-8 text-primary'>
+						Campus Chronicles
+					</h1>
 
-          <p className='text-lg text-secondary mb-10'>
-            Welcome to Campus Chronicles! Here you can find all the latest
-            newsletters. Be up to date with important news and notices. Access
-            entire catalog of past editions. And much more. You can read our
-            latest edition right now below:
-          </p>
+					<p className='text-lg text-secondary mb-10'>
+						Welcome to Campus Chronicles! Here you can find all the latest
+						newsletters. Be up to date with important news and notices. Access
+						entire catalog of past editions. And much more. You can read our
+						latest edition right now below:
+					</p>
 
-          <div className='flex gap-6'>
-            <Link
-              href={latestEdition.webViewLink}
-              className='text-xl bg-primary text-white px-4 py-3 rounded-xl shadow-xl hover-bg transition duration-200'
-              target='_blank'
-              rel='noopener noreferrer'
-            >
-              Read Latest Edition
-            </Link>
+					<div className='flex gap-6'>
+						<Link
+							href={latestEdition.webViewLink}
+							className='text-xl bg-primary text-white px-4 py-3 rounded-xl shadow-xl hover-bg transition duration-200'
+							target='_blank'
+							rel='noopener noreferrer'
+						>
+							Read Latest Edition
+						</Link>
 
-            {/* <Link
+						{/* <Link
               href='/about'
               className='bg-secondary text-amber-950 px-4 py-3 rounded-xl hover-bg transition duration-200'
               target='_blank'
@@ -74,13 +74,13 @@ export default async function Home() {
             >
               Learn More About Us
             </Link> */}
-          </div>
-        </div>
-      </div>
+					</div>
+				</div>
+			</div>
 
-      <div className='bg-secondary md:h-screen md:overflow-y-auto md:overflow-x-hidden min-h-[600px] flex-1'>
-        <Tabs newsletters={newsletters} />
-      </div>
-    </div>
-  );
+			<div className='bg-secondary md:h-screen md:overflow-y-auto md:overflow-x-hidden min-h-[600px] flex-1'>
+				<Tabs newsletters={newsletters} />
+			</div>
+		</div>
+	);
 }
